0939915cf9f1188073b3bda411f0ba7de92f115d
Sinth <sinthmite@gmail.com>
2024-07-11 16:22:20 -0400

    File name Fixes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import  {MoneyInDebit}  from './Components/Finance/MoneyInDebit/MoneyInDebit.tsx'
 
 function App() {
--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import  store from './Redux/Store/Store.ts'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
